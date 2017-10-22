faf981385e0784d9c7642ab73e3b8e756aca68e7
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 04:45:22 -0700

    tirando um aspa

--- projecto_museu_v1-master/projecto_museu_v1-master/resources/views/layouts/master.blade.php
+++ projecto_museu_v1-master/projecto_museu_v1-master/resources/views/layouts/master.blade.php
@@ -89,7 +89,7 @@
               @if (Route::has('login'))
                 @auth
                   <li class="nav-item dropdown">
-                    <a class="nav-link active dropdown-toggle" href="{{ route('logout') }}"" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">olá {{ Auth::user()->name }}</a>
+                    <a class="nav-link active dropdown-toggle" href="{{ route('logout') }}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">olá {{ Auth::user()->name }}</a>
                     <div class="dropdown-menu">
                       <a href="{{ route('home') }}">
                           Home
