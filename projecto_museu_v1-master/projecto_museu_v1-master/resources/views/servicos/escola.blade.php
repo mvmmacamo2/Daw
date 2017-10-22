103ce567d0669b9b5ad1bc90eea37af909ad8440
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-22 05:17:09 -0700

    View Pedido de Visitas Feita

--- projecto_museu_v1-master/projecto_museu_v1-master/resources/views/servicos/escola.blade.php
+++ projecto_museu_v1-master/projecto_museu_v1-master/resources/views/servicos/escola.blade.php
@@ -8,45 +8,32 @@ Requisitando Visita Guiada
 @stop
 @section('content')
 
-    <form>
+    <form method="post" action="">
+
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="data">Data Realizacao do Evento</label>
-                <input type="date" class="form-control" id="data" placeholder="">
+                <input type="date" class="form-control" id="data" placeholder="" name="data">
             </div>
             <div class="form-group col-md-6">
-                <label for="inputPassword4">Password</label>
-                <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="inputAddress">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-        </div>
-        <div class="form-group">
-            <label for="inputAddress2">Address 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" class="form-control" id="inputCity">
-            </div>
-            <div class="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select id="inputState" class="form-control">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                </select>
-            </div>
-            <div class="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
+                <label for="hora">Hora Da Visita</label>
+                <input type="text" class="form-control" id="hora" placeholder="exemplo: 10h" name="hora">
             </div>
         </div>
 
-        <button type="submit" class="btn btn-success">Sign in</button>
+        <div class="form-group">
+            <label for="inputAddress">Descricao da Visita</label>
+
+            <textarea class="form-control" cols="10" rows="5" placeholder="Coloque o motivo da Visita" name="descricao">
+
+            </textarea>
+
+        </div>
+
+
+        <button type="submit" class="btn btn-success">Submeter</button>
     </form>
 
 
 @stop
+<br>
